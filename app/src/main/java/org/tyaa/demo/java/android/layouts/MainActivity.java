71e04ef64d420b3896aab2aa6f5b03699f2795a0
YuriiTrofimenko <tyaa@ukr.net>
2021-04-27 21:46:15 +0300

    init n demo

---
+++ app/src/main/java/org/tyaa/demo/java/android/layouts/MainActivity.java
@@ -0,0 +1,15 @@
+package org.tyaa.demo.java.android.layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Custom", String.valueOf(R.layout.activity_main));
+    }
+}
